b97e9d013c08ae6be2d2bd966012a3360203c623
Matt Richmond <matt@bigdawggi.com>
2019-04-27 14:02:25 -0600

    enabling Redux Dev Tools extension

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,9 +5,13 @@ import App from './components/App.jsx';
 import { createStore } from 'redux';
 import Reducers from './reducers';
 import { Provider } from 'react-redux';
-const store = createStore(Reducers, {
-    username: 'Ann'
-});
+const store = createStore(
+    Reducers,
+    {
+        username: 'Ann'
+    },
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
     <Provider store={store}>
